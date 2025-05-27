129384aabb98b9da90a30acc256d3f1195c7bef1
Kishlaya Tiwary <kishlayatiwary@Kishlayas-MacBook-Pro.local>
2025-05-27 19:52:38 +0530

    Adding default condition

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,27 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Select the following options:");
+        int[] arr = new int[] {1, 9, 3, 8, 5, 2, 1, 7};
+        System.out.println("Select the following options to sort: " + Arrays.toString(arr));
         System.out.println("1. Bubble Sort");
         System.out.println("2. Selection Sort");
         String s = br.readLine();
         int n = Integer.parseInt(s);
-        int[] arr = new int[] {1, 9, 3, 8, 5, 2, 1, 7};
 
-        switch (s) {
-            case "1": {
+        switch (n) {
+            case 1: {
                 BubbleSort bs = new BubbleSort();
                 System.out.println(Arrays.toString(bs.bubbleSort(arr)));
                 break;
             }
-            case "2": {
+            case 2: {
                 SelectionSort ss = new SelectionSort();
                 System.out.println(Arrays.toString(ss.selectionSort(arr)));
                 break;
+            }
+            default: {
+                System.out.println("Invalid option");
+                System.out.println(Arrays.toString(arr));
             }
         }
     }
